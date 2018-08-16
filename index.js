3f17479e0658acee20923b3fe55e3abc44e08fcc
bbartolome <bbartolome@users.noreply.github.com>
2018-08-16 19:11:03 -0400

    chore: lint

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const _ = require('lodash');
 const path = require('path');
 const rjs = require('requirejs');
 
-const { version } = require('./package.json');
-
 module.exports = function mvp(argv) {
 
 	const settings = _.defaultsDeep(argv, {});
